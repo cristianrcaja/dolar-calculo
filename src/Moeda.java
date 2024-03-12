5f033032264b830d7aef4a18b01e92dec90ae162
Cristian Caja <cristianrcaja@hotmail.com>
2024-03-11 21:30:26 -0300

    projetoDolar

---
+++ src/Moeda.java
@@ -0,0 +1,21 @@
+public class Moeda {
+    public  double valorReal;
+
+    public double cotacaoDolar;
+
+    public double quantidadeDolar;
+
+    public void calcularConversao()   {
+      this.quantidadeDolar = this.valorReal / this.cotacaoDolar;
+    }
+
+     public double retornarCalculoConversao() {
+      double dolar = this.valorReal / this.cotacaoDolar;
+      return dolar;
+
+
+}
+
+
+
+
---
+++ src/TesteMoeda.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TesteMoeda {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in) ;
+        Moeda moeda = new Moeda();
+        System.out.println("Informe o valor em reais: ");
+        moeda.valorReal = leitor.nextDouble();
+        System.out.println("Informe a cotação do dolar de hoje: ");
+        moeda.cotacaoDolar = leitor.nextDouble();
+         moeda.calcularConversao();
+        System.out.println(moeda.quantidadeDolar);
+        System.out.println(moeda.retornarCalculoConversao());
+
+    }
+}
